6de4799508566ec8dce068946d1f414bd8ea1235
Luis del Llano <luisdellano@usp.br>
2022-04-29 23:35:49 -0300

    ✨ feat: pagination on find pius

--- src/modules/pius/useCases/FindPius/FindPiusController.ts
+++ src/modules/pius/useCases/FindPius/FindPiusController.ts
@@ -5,8 +5,8 @@ export class FindPiusController {
     async handle(request: Request, response: Response) {
         const createPiuUseCase = new FindPiusUseCase();
 
-        const { userId } = request;
-        const result = await createPiuUseCase.execute();
+        const { page, piusPerPage } = request.body;
+        const result = await createPiuUseCase.execute({ page, piusPerPage });
 
         return response.json(result);
     }
--- src/modules/pius/useCases/FindPius/FindPiusUseCase.ts
+++ src/modules/pius/useCases/FindPius/FindPiusUseCase.ts
@@ -1,8 +1,33 @@
 import { prisma } from "../../../../database/prismaClient";
 
+interface IFindPius {
+    page: number;
+    piusPerPage?: number;
+}
 export class FindPiusUseCase {
-    async execute() {
-        const pius = await prisma.pius.findMany();
+    async execute({ page, piusPerPage }: IFindPius) {
+        piusPerPage = piusPerPage || 10;
+
+        if (page < 0) {
+            throw new Error("page must be equal or greater than 0.");
+        }
+
+        const totalPius = await prisma.pius.count();
+        if (piusPerPage > totalPius) {
+            piusPerPage = totalPius;
+        }
+
+        let skip = page * piusPerPage - 1;
+
+        if (skip < 0) {
+            skip = 0;
+        }
+
+        const pius = await prisma.pius.findMany({
+            skip: skip,
+            take: piusPerPage,
+            orderBy: { createdAt: "desc" },
+        });
 
         return pius;
     }
